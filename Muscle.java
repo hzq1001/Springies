9d29211a69a6dfd56197420039d218ad3af9f394
JDLongley <jimmy.longley@gmail.com>
2013-02-01 14:05:46 -0500

    Update Muscle.java
    Overrode the color method so that muscles can be visually discerned from springs.

--- Muscle.java
+++ Muscle.java
@@ -1,6 +1,9 @@
 package simulation;
 
+import java.awt.Color;
 import java.awt.Dimension;
+
+import util.Vector;
 
 public class Muscle extends Spring {
 	
@@ -8,6 +11,10 @@ public class Muscle extends Spring {
 	private double myAmplitude;
     private double myPeriod;
     private double myAngle;
+    
+    //override muscle colors
+    private static Color COMPRESSED_COLOR = new Color(0,0,165,255); //dark blue
+    private static Color TENSIONED_COLOR = new Color(178,34,34,255);  //dark red
     
 	public Muscle(Mass start, Mass end, double length, double kVal, double amplitude, double period) {
 		super(start, end, length, kVal);
@@ -32,5 +39,16 @@ public class Muscle extends Spring {
 		
 		super.setLength(myRestingLength + myAmplitude*Math.sin(myAngle));
 	}
+	
+	
+	/**
+	 * Override the color method so that muscles can be visually discerned from springs.
+	 */
+	@Override
+    protected Color getColor (double diff) {
+        if (Vector.fuzzyEquals(diff, 0)) return Color.BLACK;
+        else if (diff < 0.0) return COMPRESSED_COLOR;
+        else return TENSIONED_COLOR;
+    }
 	
 }
