e84c47312c8e99e9fe15dafd6b60e1daf96447a4
Ziqiang Huang <zh28@duke.edu>
2013-02-07 12:40:45 -0500

    Implement part 3.1, 3.3

--- UserInput.java
+++ UserInput.java
@@ -1,7 +1,7 @@
 package simulation;
 
 import java.awt.event.KeyEvent;
-import java.util.Collection;
+import java.util.Map;
 
 import view.Canvas;
 
@@ -10,12 +10,36 @@ public class UserInput {
 	private Canvas myView;
 	private Model myModel;
 	
+	private Force myGravity;
+	private Force myViscosity;
+	private Force myCenterOfMass;
+	private Force myWallRepulsionTop;
+	private Force myWallRepulsionRight;
+	private Force myWallRepulsionDown;
+	private Force myWallRepulsionLeft;
+	
 	//UserInput is a Singleton
 	private static UserInput instance;
 	
+	//Default force value
+	private static final Force DEFAULT_GRAVITY = new Gravity(90, 100);
+	private static final Force DEFAULT_VISCOSITY = new Viscosity(0.1);
+	private static final Force DEFAULT_CENTEROFMASS = new CenterOfMass(100,2);
+	private static final Force DEFAULT_WALLREPULSION_TOP = new WallRepulsion(1, 500, 1.0);
+	private static final Force DEFAULT_WALLREPULSION_RIGHT = new WallRepulsion(1, 500, 1.0);
+	private static final Force DEFAULT_WALLREPULSION_DOWN = new WallRepulsion(1, 500, 1.0);
+	private static final Force DEFAULT_WALLREPULSION_LEFT = new WallRepulsion(1, 500, 1.0);
+	
 	private UserInput (Canvas view, Model model) {
-		this.myView = view;
-		this.myModel = model;
+		myView = view;
+		myModel = model;
+		myGravity = DEFAULT_GRAVITY;
+		myViscosity = DEFAULT_VISCOSITY;
+		myCenterOfMass = DEFAULT_CENTEROFMASS;
+		myWallRepulsionTop = DEFAULT_WALLREPULSION_TOP;
+		myWallRepulsionRight = DEFAULT_WALLREPULSION_RIGHT;
+		myWallRepulsionDown	= DEFAULT_WALLREPULSION_DOWN;
+		myWallRepulsionLeft = DEFAULT_WALLREPULSION_LEFT;
 	}
 	
 	public static UserInput getInstance(Canvas view, Model model)
@@ -38,12 +62,55 @@ public class UserInput {
 	}
 	
 	private void handleKeys() {
-		Collection<Integer> keys = myView.getKeysPressed();
+		int key = myView.getLastKeyPressed();
+		System.out.println(key);
 		
-		if(myView.getLastKeyPressed() == KeyEvent.VK_N) {
+		if(key == KeyEvent.VK_N) {
 			myView.loadAssembly();
+			myView.resetLastKeyPressed();
+		}
+		else if(key == KeyEvent.VK_C) {
+			myModel.clear();
 		}
 		
+		else if(key == KeyEvent.VK_G) {
+			toggleForce("gravity", myGravity);
+		}
+		
+		else if(key == KeyEvent.VK_V) {
+			toggleForce("viscosity", myViscosity);
+		}
+		
+		else if(key == KeyEvent.VK_M) {
+			toggleForce("centermass", myCenterOfMass);
+		}
+		
+		else if(key == KeyEvent.VK_1) {
+			toggleForce("wall1", myWallRepulsionTop);
+		}
+		
+		else if(key == KeyEvent.VK_2) {
+			toggleForce("wall2", myWallRepulsionRight);
+		}
+		
+		else if(key == KeyEvent.VK_3) {
+			toggleForce("wall3", myWallRepulsionDown);
+		}
+		
+		else if(key == KeyEvent.VK_4) {
+			toggleForce("wall4", myWallRepulsionLeft);
+		}
+	}
+
+	private void toggleForce(String name, Force f) {
+		Map<String, Force> m = myModel.getMyForceMap();
+		if(m.containsKey(name)) {
+			f = m.get(name);
+			m.remove(name);
+		} else {
+			m.put(name, f);
+		} 
+		myView.resetLastKeyPressed();
 	}
 	private void handleMouse() {
 		
